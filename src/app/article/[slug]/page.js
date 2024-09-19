51812a3d7ccbc887abb77282c15a18af772440af
Cui <cl6293@nyu.edu>
2024-09-19 09:28:16 -0400

    Updated files

---
+++ src/app/article/[slug]/page.js
@@ -0,0 +1,8 @@
+export default function Article({ params }){
+    console.log("params:", params);
+    return (
+        <main>
+            <h1>Article: {params.slug}</h1>
+        </main>
+    );
+}

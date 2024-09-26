1520348d1f560701bc41bd02a410728426e4f76a
Cui <cl6293@nyu.edu>
2024-09-26 08:08:49 -0400

    Updated Article Pages and Dates

--- src/app/components/articleCard.js
+++ src/app/components/articleCard.js
@@ -1,13 +1,16 @@
 import Link from "next/link";
 import styles from "../page.module.css";
+import { formatDateForArticle } from "./utils";
 
 export default function ArticleCard({ blurb, image, id, publishedDate, title }) {
+    const freshDate = new Date(publishedDate);
+    const formattedDate = `${freshDate.getMonth() + 1}/${freshDate.getDay()}/${freshDate.getFullYear()}`;
     return (
         <div className={styles.articleCardWrapper}>
             <img src={image.url} alt={image.alt} />
             <div className={styles.articleCardText}>
                 <h2>{title}</h2>
-                <p className={styles.articleCardDate}>{publishedDate}</p>
+                <p className={styles.articleCardDate}>{formatDateForArticle(publishedDate)}</p>
                 <p>{blurb}</p>
                 <Link href={`article/${id}`}>Read More</Link>
             </div>
--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,6 @@ export default function Home() {
         id={article.id}
         publishedDate={article.publishedDate} 
         title={article.title}
-        
         />
         ))}
     </main>
